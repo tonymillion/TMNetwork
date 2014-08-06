a47d2d8c43cb4bc212f1bcbb3c0ea91b73d086c4
Tony Million <tony.million@blackwelllearning.co.uk>
2014-08-06 15:39:38 +0100

    move TMNetwork->Classes added podspec added license

--- Classes/TMNetworkActivityIndicatorManager.m
+++ Classes/TMNetworkActivityIndicatorManager.m
@@ -73,7 +73,7 @@
     else 
     {
         // we wait a wee while after hitting 0
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             
             // if the count is STILL 0 then we'll turn it off
             if(self.count == 0)
